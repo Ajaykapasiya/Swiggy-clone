27d1573b77faadd337e25c92fb21db0b85f56e3d
ajay <ajaykumar290904@gmail.com>
2024-10-18 10:01:02 +0530

    project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,8 @@ function Header() {
 
     {
       icon: <CiDiscount1 />,
-      name: "Offers"
+      name: "Offers",
+      sup:" New"
     },
 
     {
@@ -55,18 +56,30 @@ function Header() {
   return (
 
     <>
-      
+      <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu} style={{
+        opacity: toggle ? 1 : 0,
+        visibility: toggle ? "visible" : "hidden"
+      }}>
+
+        <div onClick={(e) => {
+          e.stopPropagation();
+        }} className='w-[400px] bg-white h-full absolute  duration-[300ms]'
+
+          style={{
+            left: toggle ? '0%' : '-100%'
+          }}
+        ></div>
 
       </div>
 
 
       <header className="p-[15px] shadow xl">
-        <div className='max-w-[1200px] mx-auto border border-blue-500  flex items-center'>
-          <div className='w-[80px] border '>
+        <div className='max-w-[1200px] mx-auto   flex items-center'>
+          <div className='w-[80px]  '>
             <img src="images/logo.png" className='w-full' alt="" />
           </div>
           <div className=''>
-            <span className=' font-bold border-b-[3px] border-[black]'>Greater Noida</span> , Uttar pardesh  <PiCaretDownBold onClick={showSideMenu} fontSize={25} className='inline text- text-[#ff5200]' />
+            <span className=' font-bold  border-b-[3px]  '>Greater Noida</span> , Uttar pardesh  <PiCaretDownBold onClick={showSideMenu} fontSize={25} className='inline text- text-[#ff5200]' />
           </div>
           <nav className='flex list-none gap-4 ml-auto font-semibold text-[18px]'>
 
@@ -76,6 +89,7 @@ function Header() {
                   return <li key = {index} className='flex hover:text-[#fc8019] items-center gap-3 '>
                     {link.icon}
                     {link.name}
+                    <sup>{link.sup}</sup>
                   </li>
                 }
               )
