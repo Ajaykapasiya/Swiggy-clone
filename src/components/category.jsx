dcdd0670ebc2e19fe38baac117bb9f2a76a30271
ajay <ajaykumar290904@gmail.com>
2024-10-22 20:40:31 +0530

    project

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -18,6 +18,16 @@ useEffect(
     },[]
 )
 
+    return (
+        <div className='max-w-[1200px] mx-auto ' >
+            <div className='flex items-center justify-between'>
+                <div className='text-[30px] font-bold '>Ajay, what's on your mind?</div>
+                <div className='flex'>
+                    <div className=' cursor-pointer flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2'><HiArrowSmLeft size={24} /></div>
+                    <div className=' cursor-pointer  flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2'><HiArrowSmRight size={24} /></div>
+                </div>
+
+            </div>
 
             <div className='flex'>
 
