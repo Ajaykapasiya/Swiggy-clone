19d86302ebadb34ab92cf73c57c4458ef09e90cb
ajay <ajaykumar290904@gmail.com>
2024-11-06 10:44:48 +0530

    project

--- src/components/TopRes.jsx
+++ src/components/TopRes.jsx
@@ -1,43 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card';
 export default function TopRes() {
-const [data , setData] = useState([]);
+    const [data, setData] = useState([]);
 
-const fetchTopRestaurant =async () => {
-const response = await fetch ('http://127.0.0.1:5500/data/restaurantChains.json')
-const apiData = await response.json ();
-setData(apiData);
-}
+    const fetchTopRestaurant = async () => {
+        const response = await fetch('http://127.0.0.1:5500/data/restaurantChains.json')
+        const apiData = await response.json();
+        setData(apiData);
+    }
 
-useEffect (
+    useEffect(
 
-    () => {
-    fetchTopRestaurant();
-    } , []
-) 
+        () => {
+            fetchTopRestaurant();
+        }, []
+    )
 
-  return (
-    <div>
-       <div className=' max-w-[1200px] mx-auto ' >
-            <div className='flex my-5 items-center justify-between'>
-                <div className='text-[30px] font-bold '>Top Resturant in Greater Noida </div>
-                <div className='flex'>
-                    <div className=' cursor-pointer flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' >
-                       </div>
-
-                    <div className=' cursor-pointer  flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' >
+    return (
+        <div>
+            <div className=' max-w-[1200px] mx-auto ' >
+                <div className='flex my-5 items-center justify-between'>
+                    <div className='text-[30px] font-bold '>Top restaurant chains in Noida
+                    </div>
+                    <div className='flex'>
+                        <div className=' cursor-pointer flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' >
                         </div>
+
+                        <div className=' cursor-pointer  flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' >
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='flex '>
+                    <Card />
+
+
                 </div>
 
             </div>
-
-            <div className =  'flex '>
-                <Card/>
-                
-
-            </div>
-
-       </div>
-    </div>
-  ) 
+        </div>
+    )
 }
