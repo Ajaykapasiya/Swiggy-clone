971676ee14613a96c5a4554bc5fca38da08c199b
ajay <ajaykumar290904@gmail.com>
2024-11-06 13:23:22 +0530

    Restaurant card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-export default function Card() {
-  return (
+export default function Card(props) {
+    return (
 
-    <div className = ' w-[200px] overflow-hidden '>
-        <div className =' w-full h-[182px] rounded-[15px] overflow-hidden relative'>
-      <img className='w-full h-full object-cover ' src="http://127.0.0.1:5500/public/images/2b4f62d606d1b2bfba9ba9e5386fabb7.jpeg" alt="" />
-      <div className='image-overlay absolute w-full h-full '></div>
-      </div>
-    </div>
-  )
+        <div className='  w-[273px] shrink-0 grow '>
+            <div className='h-[182px] rounded-[15px] overflow-hidden relative '>
+
+                <img className='object-cover w-full h-full ' src="http://127.0.0.1:5500/public/images/2b4f62d606d1b2bfba9ba9e5386fabb7.jpeg" alt="" />
+
+                <div className='image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[20px] text-white font-bold tracking-tighter cursor-pointer'>
+                Items at ₹179
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/components/TopRes.jsx
+++ src/components/TopRes.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card';
+import { HiArrowSmRight, HiArrowSmLeft } from "react-icons/hi";
 export default function TopRes() {
+
+    const [slide, setSlide] = useState(0);
+
     const [data, setData] = useState([]);
 
     const fetchTopRestaurant = async () => {
@@ -16,6 +20,16 @@ export default function TopRes() {
         }, []
     )
 
+    const nextSlide = () => {
+        if (data.length - 8 == slide) return false;
+        setSlide(slide + 3);
+    }
+
+    const prevSlide = () => {
+        if (slide == 0) return false
+        setSlide(slide - 3);
+    }
+
     return (
         <div>
             <div className=' max-w-[1200px] mx-auto ' >
@@ -23,19 +37,25 @@ export default function TopRes() {
                     <div className='text-[30px] font-bold '>Top restaurant chains in Noida
                     </div>
                     <div className='flex'>
-                        <div className=' cursor-pointer flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' >
-                        </div>
+                        <div className=' cursor-pointer flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' onClick={prevSlide} >
+                            <HiArrowSmLeft  size={24} /> </div>
 
-                        <div className=' cursor-pointer  flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' >
-                        </div>
+                        <div className=' cursor-pointer  flex justify-center items-center w-[32px] h-[30px] bg-[#02060c26] rounded-full mx-2' onClick={nextSlide}>
+                            <HiArrowSmRight  size={24} /> </div>
                     </div>
 
                 </div>
 
-                <div className='flex '>
-                    <Card />
-
-
+                <div className='flex gap-3 overflow-hidden'>
+                   {
+                    data.map(
+                        (d , i) => {
+                            return <Card {...d} key={i}/>
+                        }
+                    )
+                   }
+                    
+                  
                 </div>
 
             </div>
